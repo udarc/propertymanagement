b5936e3140464d8350ec9f205ac05f1e0ac2d5c2
udarc <juwimana@madisoncollege.edu>
2020-06-22 23:59:21 -0500

    added links to change and reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path('register', UserRegistrationView.as_view(), name='register'),
     path('login', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('change-password/', auth_views.PasswordChangeView.as_view()),
+    path('reset-password/',auth_views.PasswordResetView.as_view(),name=" password_reset"),
+    path('reset-password-sent/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('reset-success/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
          # User Profile
 
     path('profile/<slug:slug>/',
